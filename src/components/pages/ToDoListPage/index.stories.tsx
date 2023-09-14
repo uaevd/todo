d1009fc8152f861ffda8d5d2da8e7b152613aec4
uaevd <saekkigae45@gmail.com>
2023-09-14 16:19:02 +0700

    Add ToDoList

--- src/components/pages/ToDoListPage/index.stories.tsx
+++ src/components/pages/ToDoListPage/index.stories.tsx
@@ -1,3 +1,4 @@
+import { BrowserRouter } from 'react-router-dom';
 import { Meta, StoryObj } from '@storybook/react';
 
 import { ToDoListPage } from '.';
@@ -5,6 +6,13 @@ import { ToDoListPage } from '.';
 export default {
     title: 'Pages/ToDoListPage',
     component: ToDoListPage,
+    decorators: [
+        (Story) => (
+            <BrowserRouter>
+                <Story />
+            </BrowserRouter>
+        ),
+    ],
 } as Meta<typeof ToDoListPage>;
 
 type Story = StoryObj<typeof ToDoListPage>;
--- src/components/pages/ToDoListPage/index.tsx
+++ src/components/pages/ToDoListPage/index.tsx
@@ -1,3 +1,7 @@
+import { ToDoList } from 'components/templates/ToDoList';
+
 export const ToDoListPage = () => {
-    return <div>ToDoListPage</div>;
+    const toDoList = ['Study React.js', 'Study CDD', 'Create To-Do List App'];
+
+    return <ToDoList toDoList={toDoList} />;
 };
