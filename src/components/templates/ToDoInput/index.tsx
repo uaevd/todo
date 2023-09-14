2c2919a2246e361051fa27eee98377f36daeca2e
uaevd <saekkigae45@gmail.com>
2023-09-14 16:09:35 +0700

    Apply CSS-in-JS

--- src/components/templates/ToDoInput/index.tsx
+++ src/components/templates/ToDoInput/index.tsx
@@ -1,17 +1,43 @@
+import styled from '@emotion/styled';
+
 import { PageTitle } from 'components/atoms/PageTitle';
 import { Button } from 'components/atoms/Button';
 import { InputToDo } from 'components/organisms/InputToDo';
 
+const Container = styled.div`
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    background-color: #eee;
+`;
+
+const Contents = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    background-color: #fff;
+    padding: 32px;
+    border-radius: 8px;
+`;
+
+const ButtonContainer = styled.div`
+    position: absolute;
+    inset: auto 40px 40px auto;
+`;
+
 export const ToDoInput = () => {
     return (
-        <div>
-            <div>
+        <Container>
+            <Contents>
                 <PageTitle title="Add To-Do Item" />
                 <InputToDo />
-            </div>
-            <div>
+            </Contents>
+            <ButtonContainer>
                 <Button label="Close" />
-            </div>
-        </div>
+            </ButtonContainer>
+        </Container>
     );
 };
